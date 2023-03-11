8516988bf3aff7af65263c383d2d3e38ae97e066
Mads Cordes <Mobilpadde@gmail.com>
2023-03-11 22:44:02 +0100

    * 🎉 feat(letters.go): add constants for lowercase, uppercase, and all letters * 🎉 feat(numbers.go): add constants for all, even, odd, and zero numbers The `letters` package now contains constants for lowercase, uppercase, and all letters. The `numbers` package now contains constants for all, even, odd, and zero numbers. These constants can be used in various applications that require specific sets of letters or numbers.

---
+++ letters/letters.go
@@ -0,0 +1,12 @@
+package letters
+
+const (
+	// Lowercase contains all lowercase letters from `a` to `z`.
+	Lowercase = "abcdefghijklmnopqrstuvwxyz"
+
+	// Uppercase contains all uppercase letters from `A` to `Z`.
+	Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	// All contains all lowercase and uppercase letters from above.
+	All = Lowercase + Uppercase
+)
---
+++ numbers/numbers.go
@@ -0,0 +1,15 @@
+package numbers
+
+const (
+	// All contains all single numbers from `0` to `9`.
+	All = "0123456789"
+
+	// Even contains all even numbers.
+	Even = "02468"
+
+	// Odd contains all odd numbers.
+	Odd = "13579"
+
+	// Zero only contains `0`.
+	Zero = "0"
+)
